8394b764d67c8365dbd6100344c52643d07428ce
Utkarsh <utkarshchoudhary1573@gmail.com>
2023-04-03 01:10:17 +0530

    updated!

--- js/script.js
+++ js/script.js
@@ -78,3 +78,32 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
 ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form', { origin: 'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left'});
 ScrollReveal().reveal('.home-content h3,.home-content p, .about-content', { origin: 'right'});
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyjw32cUr2-O93qY9Cyk5UfuwRwfAArBX3SOyqRGuDluV0PLUpQXPldIDpmUY5UCJHCzA/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    // console.log("Entered form");-
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successful!";
+            // console.log("Msg changed");
+            setTimeout(function (){
+                msg.innerHTML = ""
+            }, 3000);
+    form.reset
+})
+    .catch(error => console.error('Error!', error.message))
+})
+
+
+
+
+
+
+
+
+
